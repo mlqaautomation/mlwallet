c1297fbcf5707a4d27fb405deb4707be94d29c6d
qa-dominic <dominic.melgo@mlhuillier.com>
2023-11-23 13:52:55 +0800

    update class on logout

--- src/test/java/com/mlwallet/regression/MLWalletLogoutScripts.java
+++ src/test/java/com/mlwallet/regression/MLWalletLogoutScripts.java
@@ -1,15 +1,16 @@
 package com.mlwallet.regression;
 
-import com.business.mlwallet.MLWalletBusinessLogic;
+import com.business.mlwallet.LogoutClass;
 import com.driverInstance.AppiumServer;
+import jdk.jpackage.internal.Log;
 import org.testng.annotations.*;
 
 public class MLWalletLogoutScripts {
 
     public static String deviceName;
     public static String portno;
-    public  static com.business.mlwallet.MLWalletBusinessLogic MLWalletBusinessLogic;
-
+    public static LogoutClass logoutClass;
+    
 
 
     //@BeforeSuite(groups = { "All" })
@@ -19,7 +20,7 @@ public class MLWalletLogoutScripts {
         AppiumServer.startServer();
         MLWalletLogoutScripts.deviceName=deviceName;
         MLWalletLogoutScripts.portno= portno;
-        MLWalletBusinessLogic = new MLWalletBusinessLogic("MLWallet",deviceName,portno);
+        logoutClass = new LogoutClass("MLWallet",deviceName,portno);
     }
 
 //=======================================================================================================================//
@@ -27,37 +28,37 @@ public class MLWalletLogoutScripts {
 
     @Test(priority = 1)
     public void logOutMinimizeAndRelaunch_Lout_TC_03() throws Exception {
-        MLWalletBusinessLogic.logOutMinimizeAndRelaunch_Lout_TC_03();
+        logoutClass.logOutMinimizeAndRelaunch_Lout_TC_03();
     }
 
     @Test(priority = 2)
     public void logOutAppKillAndRelaunch() throws Exception {
-        MLWalletBusinessLogic.logOutAppKillAndRelaunch_Lout_TC_04();
+        logoutClass.logOutAppKillAndRelaunch_Lout_TC_04();
     }
 
     @Test(priority = 3)
     public void logOUtPopUpValidation_Lout_TC_05() throws Exception {
-        MLWalletBusinessLogic.logOUtPopUpValidation_Lout_TC_05();
+        logoutClass.logOUtPopUpValidation_Lout_TC_05();
     }
 
     @Test(priority = 4)
     public void logOutPopUpCancelBtnValidation_Lout_TC_06() throws Exception {
-        MLWalletBusinessLogic.logOutPopUpCancelBtnValidation_Lout_TC_06();
+        logoutClass.logOutPopUpCancelBtnValidation_Lout_TC_06();
     }
 
     @Test(priority = 5)
     public void logOutPopUpLogOutBtnValidation_Lout_TC_07() throws Exception {
-        MLWalletBusinessLogic.logOutPopUpLogOutBtnValidation_Lout_TC_07();
+        logoutClass.logOutPopUpLogOutBtnValidation_Lout_TC_07();
     }
 
     @Test(priority = 6)
     public void logoutChangeNumberUIValidation_Lout_TC_08() throws Exception {
-        MLWalletBusinessLogic.logoutChangeNumberUIValidation_Lout_TC_08();
+        logoutClass.logoutChangeNumberUIValidation_Lout_TC_08();
     }
 
     @Test(priority = 7)
     public void logInWithChangeNumber_Lout_TC_09() throws Exception {
-        MLWalletBusinessLogic.logInWithChangeNumber_Lout_TC_09();
+        logoutClass.logInWithChangeNumber_Lout_TC_09();
     }
 
     @AfterMethod
