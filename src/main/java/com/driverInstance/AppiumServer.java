1810554f96ffe5df598a05f0477ade5957edb9aa
mlqaautomation <mlqaautomation@mlhuillier.com>
2024-01-17 14:51:01 +0800

    appium class update

--- src/main/java/com/driverInstance/AppiumServer.java
+++ src/main/java/com/driverInstance/AppiumServer.java
@@ -15,63 +15,49 @@ public class AppiumServer{
    public AppiumServer(){
 
    }
-      public static LoggingUtils logger = new LoggingUtils();
-      static AppiumDriverLocalService service;
-      public static String osName=System.getProperty("os.name").toLowerCase();
-      private static String getAppiumJSPath() {
-         String userName = System.getenv("USERNAME");
-         return "C:\\Users\\MELG20229793\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
-   }
+   public static LoggingUtils logger = new LoggingUtils();
+   private static AppiumDriverLocalService service;
+   public static String osName=System.getProperty("os.name").toLowerCase();
+   private static final String NODE_PATH_LINUX = "/usr/bin/node";
+   private static final String APPIUM_JS_PATH_LINUX = "/usr/bin/appium";
+   private static final String NODE_PATH_WINDOWS = "C:\\Program Files\\nodejs\\node.exe";
+   private static final String APPIUM_JS_PATH_WINDOWS = "C:\\npm\\node_modules\\appium\\build\\lib\\main.js";
+   private static final String IP_ADDRESS = "127.0.0.1";
+   private static final int[] PORTS = { 4723, 4725, 4727, 4729, 4731, 4733, 4735, 4737, 4739, 4741 };
+   //   private static String getAppiumJSPath() {
+   //      return "C:\\npm\\node_modules\\appium\\build\\lib\\main.js";
+   //   }
    public static void startServer() throws IOException {
-      int[] ports ={4723, 4725, 4727, 4729, 4731, 4733, 4735, 4737, 4739, 4741};
       uninstallAppiumServer();
-      switch (osName){
-         case "linux":
-               String nodePath = "/usr/bin/node"; // Path to the Node.js executable
-               String appiumJSPath = "/usr/bin/appium"; // Path to the Appium main.js file
-               for(int port : ports){
-                   service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
-                          .usingDriverExecutable(new File(nodePath))
-                          .withAppiumJS(new File(appiumJSPath))
-                          .withIPAddress("127.0.0.1")
-                          .usingPort(port)
-                          .withArgument(GeneralServerFlag.LOG_LEVEL
-                  ));
-               }
-               if (service.isRunning()) {
-                  service.stop();
-               } else {
-                  service.start();
-                  service.clearOutPutStreams();
-                  logger.info("[EVENT] Appium Server Started Sucessfully.");
-               }
-            break;
-         default:
-            for(int port : ports){
-               service = AppiumDriverLocalService.
-                       buildService(new AppiumServiceBuilder()
-                               .usingDriverExecutable(new File("C:\\Program Files\\nodejs\\node.exe"))
-                               .withAppiumJS(new File(getAppiumJSPath()))
-                               .withIPAddress("127.0.0.1").usingPort(port)
-                               .usingDriverExecutable (new File ("C:\\Program Files\\nodejs\\node.exe"))
-                               .withArgument(GeneralServerFlag.LOG_LEVEL, "error"
-                      ));
-            }
-            if (service.isRunning()) {
-               service.stop();
-            } else {
-               service.start();
-               logger.info(getAppiumJSPath());
-               logger.info("[EVENT] Appium Server Started Successfully.");
-            }
-         }
+      String nodePath = osName.startsWith("linux") ? NODE_PATH_LINUX : NODE_PATH_WINDOWS;
+      String appiumJSPath = osName.startsWith("linux") ? APPIUM_JS_PATH_LINUX : APPIUM_JS_PATH_WINDOWS;
+      for (int port : PORTS) {
+         service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                 .usingDriverExecutable(new File(nodePath))
+                 .withAppiumJS(new File(appiumJSPath))
+                 .withIPAddress(IP_ADDRESS)
+                 .usingPort(port)
+                 .withArgument(GeneralServerFlag.LOG_LEVEL, "error"));
+      }
+
+      if (service.isRunning()) {
+         service.stop();
+      } else {
+         service.start();
+         service.clearOutPutStreams();
+         logger.info("[EVENT] Appium Server Started Successfully.");
+      }
    }
 
-   private static void uninstallAppiumServer() throws IOException {
-      String cmd1 = "adb uninstall io.appium.uiautomator2.server";
-      String cmd2 = "adb uninstall io.appium.uiautomator2.server.test";
-      Runtime.getRuntime().exec(cmd1);
-      Runtime.getRuntime().exec(cmd2);
+   private static void uninstallAppiumServer(){
+      try {
+         String cmd1 = "adb uninstall io.appium.uiautomator2.server";
+         String cmd2 = "adb uninstall io.appium.uiautomator2.server.test";
+         Runtime.getRuntime().exec(cmd1);
+         Runtime.getRuntime().exec(cmd2);
+      } catch (IOException e) {
+         logger.info("Failed to uninstall Appium server." + e);
+      }
    }
    public static void stopServer(){
       DriverManager.getAppiumDriver().quit();
